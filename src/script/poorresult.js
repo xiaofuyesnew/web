afad7658ac2d0b1cba1ef3580847d9a5eb467201
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-07-13 16:18:13 +0800

    new

---
+++ src/script/poorresult.js
@@ -0,0 +1,11 @@
+$(() => {
+    var app = {
+        el: $('#app'),
+        setScreen: () => {
+            app.el.css({"height": `${window.innerHeight - 20}px`})
+        }
+    }
+
+    //调用方法
+    app.setScreen()
+})
